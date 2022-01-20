29f815618857b3310408429b75d02a0d99e561a9
Mateo <mtaycruz@gmail.com>
2022-01-20 11:18:25 -0500

    update mainfeed with template code

--- src/components/Feed/index.js
+++ src/components/Feed/index.js
@@ -1,1 +1,29 @@
-// placeholder file
+import React, { Component } from 'react'
+
+class MainFeed extends Component() {
+
+    constructor(){
+        super();
+        this.state = {
+            
+        }
+    }
+    render(){
+
+        return(
+            <div>
+                <h1>News</h1>
+                <div>
+                <h3>Crypto</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vulputate felis vel augue semper efficitur. Phasellus pretium nisl sit amet neque eleifend dapibus. Duis ac elementum sem. Donec efficitur metus tincidunt, sodales augue et, mattis urna. Phasellus consectetur,</p>
+                </div>
+            </div>
+        )
+
+    }
+ 
+ 
+
+}
+
+export default MainFeed;
