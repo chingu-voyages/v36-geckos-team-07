c5ee813a97205c6d50b59987aecc338a222879f9
Gift <56089461+e-giftz@users.noreply.github.com>
2022-01-20 23:35:45 -0500

    initial bootstrap navbar added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+// import { Navbar } from 'react-bootstrap';
 import './App.css';
+import NavBar from './components/Navbar/index'
 import MainFeed from './components/Feed/index'
 import './css/main.css';
-// import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 
 function App() {
   return (
     <div>
+      <NavBar />
       <div>
         <MainFeed></MainFeed>
       </div>
