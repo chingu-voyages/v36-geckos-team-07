9c38af631484183d3be79eb48d662265d968d531
Mateo <mtaycruz@gmail.com>
2022-01-21 13:36:30 -0500

    API data now displays on webpage

--- src/components/Feed/index.js
+++ src/components/Feed/index.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react'
-import { NewsCard } from './newsCard/newsCard';
+import { NewsCardList } from './news-card-list/news-card-list';
 
 class MainFeed extends Component {
+    constructor(){
+        super()
 
+        this.state = {
+            news: []
+        }
+    }
 
     componentDidMount(){
-        fetch("https://cryptocurrency-news-live1.p.rapidapi.com/NFT_DeFi", {
+        fetch("https://cryptocurrency-news-live1.p.rapidapi.com/news", {
             "method": "GET",
             "headers": {
                 "x-rapidapi-host": "cryptocurrency-news-live1.p.rapidapi.com",
@@ -14,8 +20,9 @@ class MainFeed extends Component {
         })
         .then(response => {
             if(
+                // check if response's status is 200
                 response.ok && 
-
+                // check if API return data is in JSON format
                 response.headers.get('Content-Type').includes('application/json')
             ){
                 return response.json()
@@ -24,7 +31,7 @@ class MainFeed extends Component {
             }
         })
         .then(data => {
-            console.log(data)
+            this.setState({ news: data })
         })
         .catch(err => {
             console.error(err);
@@ -37,7 +44,10 @@ class MainFeed extends Component {
 
         return(
             <div>
-                <NewsCard></NewsCard>
+                <NewsCardList news={this.state.news}>
+
+                </NewsCardList>
+            
             </div>
         )
 
