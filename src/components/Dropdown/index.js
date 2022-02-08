f8cb65d0587e8c07afdf818c430fd6f7b58ce6bd
Mateo <mtaycruz@gmail.com>
2022-02-07 21:27:50 -0500

    update from development

--- src/components/Dropdown/index.js
+++ src/components/Dropdown/index.js
@@ -1,7 +1,5 @@
 import React from "react";
 
-import '../../sass/components/_dropdown.scss';
-
 export const DropDown = (props) => (
     <form 
         action={props.action}
--- src/components/Feed/index.js
+++ src/components/Feed/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { NewsCardList } from './news-card-list/news-card-list';
-import { DropDown, Option} from '../Dropdown';
+import { DropDown, Option} from '../';
 import TrendingNftsList from '../TrendingNFTs/nft-card-list/nft-card-list';   
 
 class MainFeed extends Component {
@@ -118,7 +118,7 @@ class MainFeed extends Component {
                 
                 </div>
             
-            </div>
+            </>
         )
 
     }
